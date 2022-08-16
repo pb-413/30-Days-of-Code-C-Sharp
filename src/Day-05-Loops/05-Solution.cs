d646286e7ff4d2d1130ffa6e2de90e11a5870028
Patrick Byers <byers@lucasware.com>
2022-08-16 11:34:50 -0400

    rename day 4 to ClassVsInstance and set up for day 5

---
+++ src/Day-05-Loops/05-Solution.cs
@@ -0,0 +1,31 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+
+    printTenMutliples(int input)
+    {
+        
+    }
+
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        printTenMultiples(n);
+    }
+}
