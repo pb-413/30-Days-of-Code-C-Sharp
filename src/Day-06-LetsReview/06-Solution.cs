94f5293c7d67a8e96cb4268aa8e57c63286fb4b2
Patrick Byers <byers@lucasware.com>
2022-08-18 18:48:02 -0400

    setup read-in loop like day 4 and outline solution carrying meathod

--- src/Day-06-LetsReview/06-Solution.cs
+++ src/Day-06-LetsReview/06-Solution.cs
@@ -2,7 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 class Solution {
+
+    public static void printEvenThenOddCharacters()
+    {
+        //build string(s) w/ loop + if-else
+
+        //print string(s)
+    }
+
     static void Main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int num_test_cases=int.Parse(Console.In.ReadLine());
+        for (int i = 0; i < num_test_cases; i++) {
+            string str=Console.In.ReadLine();
+
+            printEvenThenOddCharacters(str);
+
+        }
+    
     }
 }
