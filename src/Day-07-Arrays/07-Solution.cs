2156ddeb926b2bb48302fc4639cacdba49e4d67f
Patrick Byers <byers@lucasware.com>
2022-08-22 19:30:10 -0400

    expect to fail and be printing on new lines

--- src/Day-07-Arrays/07-Solution.cs
+++ src/Day-07-Arrays/07-Solution.cs
@@ -12,14 +12,36 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
+/*
 
+Constraints 
+
+N - Number of elements in the array ranges from 1 to 1000
+
+A[i] - the ith element of the array ranges from 1 to 10000
+
+*/
 
 class Solution
 {
+
+    public static void printReverse(List<int> input)
+    {
+        
+        for(int itemPosition = 0; itemPosition < input.Count(); itemPosition++)
+        {
+            //Constole.WriteLine(input.RemoveAt(-1));
+            Console.WriteLine(input[itemPosition]);
+        }
+        
+    }
+
     public static void Main(string[] args)
     {
         int n = Convert.ToInt32(Console.ReadLine().Trim());
 
         List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        printReverse(arr);
     }
 }
