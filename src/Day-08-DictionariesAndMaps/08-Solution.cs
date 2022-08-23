71d4716dd89183896655f02605ded3b02a4a1877
Patrick Byers <byers@lucasware.com>
2022-08-23 16:21:30 -0400

    initial outline w/ TODOs

--- src/Day-08-DictionariesAndMaps/08-Solution.cs
+++ src/Day-08-DictionariesAndMaps/08-Solution.cs
@@ -1,8 +1,42 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+
 class Solution {
+    
+    public static void queryPhonebook(string query, Dictionary phonebook)
+    {
+        Console.WriteLine(phonebook[query]);
+
+        //TODO process null return (i'm guessing)
+    }
+    
     static void Main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+
+        
+        /*Read in N, the number of phonebook entries*/
+
+        int numEntries = int.Parse(Console.In.ReadLine());
+        
+        
+        /*Read in N # of entries and create the phonebook*/
+
+        Dictionary<string,int> phonebook;
+
+        for (int i = 0; i < numEntries; i++) 
+        {
+            //TODO process input and prepare for entry into phonebook (dict)
+            
+            phonebook.Add();
+        }
+
+        
+        /*Read in quieries for the phonebook and return results*/
+
+        //TODO while there is still input
+        string query = ""; //TODO parse input
+        queryPhonebook(query, phonebook);
+    
     }
 }
