94ac35938cefcca8e52e98e933213e4335598d7d
Patrick Byers <byers@lucasware.com>
2022-08-30 15:36:19 -0400

    remove leftover from string-y phone number

--- src/Day-08-DictionariesAndMaps/08-Solution.cs
+++ src/Day-08-DictionariesAndMaps/08-Solution.cs
@@ -11,7 +11,7 @@ class Solution {
         
         try
         {
-            number = (string) phonebookDict[query];
+            number = phonebookDict[query];
         }
         catch (System.Collections.Generic.KeyNotFoundException e )
         {
