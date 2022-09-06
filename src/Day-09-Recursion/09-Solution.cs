3922142f75be7450a1668232491d0014c10678ac
Patrick Byers <byers@lucasware.com>
2022-09-06 19:51:40 -0400

    first draft of factorial

--- src/Day-09-Recursion/09-Solution.cs
+++ src/Day-09-Recursion/09-Solution.cs
@@ -24,7 +24,14 @@ class Result
 
     public static int factorial(int n)
     {
-
+        if (n <= 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n* factorial(n-1);
+        }
     }
 
 }
