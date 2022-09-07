c60750875fc219762975b0ebf13b73690cd2a576
Patrick Byers <byers@lucasware.com>
2022-09-06 20:22:29 -0400

    init day 10 and first draft of solution

---
+++ src/Day-10-BinaryNumbers/10-Solution.cs
@@ -0,0 +1,63 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+
+    public static class CustomNumber
+    {
+        public CustomNumber(int input)
+        {
+            int decimalRepresentation = input;
+            string binaryRepresentation = Convert.ToString(decimalRepresentation, 2); // UNDONE cast binary
+            int maxConsecutiveOnesInBinaryRepresentation = GetMaxConsecutiveOnes(binaryRepresentation);
+        }
+
+        public static int GetMaxConsecutiveOnes(string binaryNumber)
+        {
+            string [] arrayOfSpans = binaryNumber.Split("0");
+
+            int maxSpan = 0;
+
+            for (int binarySpan = 0; binarySpan < arrayOfSpans.Length; binarySpan++)
+            {
+                int lengthOfCurrentSpan = arrayOfSpans[binarySpan].Length;
+                
+                if (maxSpan < lengthOfCurrentSpan)
+                {
+                    maxSpan = lengthOfCurrentSpan;
+                }
+            }
+
+            return maxSpan;
+        }
+
+        public static void PrintMaxConsecutiveOnesInBinaryRepresentation()
+        {
+            Console.WriteLine(maxConsecutiveOnesInBinaryRepresentation);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        CustomNumber num = new CustomNumber(n);
+
+        num.PrintMaxConsecutiveOnesInBinaryRepresentation();
+
+    }
+}
